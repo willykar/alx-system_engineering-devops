941802fdca22c8657cf79d31c4e7b7a931c8f740
willykar <wilsonnjenga799@gmail.com>
2024-03-22 11:56:21 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # install flask from pip3 using Puppet
-class { 'python::pip': }
+
 package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
-require  => Class['python::pip'],
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
