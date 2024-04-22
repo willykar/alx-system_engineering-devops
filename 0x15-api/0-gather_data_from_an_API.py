9e0c736be974df22de4e06514d9a8f5c3c573fc7
willykar <wilsonnjenga799@gmail.com>
2024-04-22 11:19:34 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,13 +5,16 @@ by their id"""
 import requests
 from sys import argv
 
-if len(argv) != 2:
-    print("Usage: python3 script.py <employee_id>")
-    exit(1)
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        exit(1)
 
-employee_id = argv[1]
+    employee_id = argv[1]
 
-user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+user_response =
+requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}")
 if user_response.status_code != 200:
     print(f"Errot: unable to fetch user details for ID {employee_id}")
     exit(1)
@@ -20,7 +23,9 @@ user_data = user_response.json()
 employee_name = user_data.get('name')
 
 # Fetch user's TODo list
-todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+todos_response =
+requests.
+get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
 if todos_response.status_code != 200:
     print(f"Error: unable to fetch TODO list for ID {employee_id}")
     exit(1)
@@ -33,6 +38,7 @@ total_tasks = len(todos_data)
 completed_count = len(completed_tasks)
 
 # Display employee TODO list
-print(f"Employee {employee_name} is done with tasks({completed_count}/{total_tasks}):")
+print(f"Employee {employee_name} is done with
+      tasks({completed_count}/{total_tasks}):")
 for task in completed_tasks:
     print(f"{task['title']}")
