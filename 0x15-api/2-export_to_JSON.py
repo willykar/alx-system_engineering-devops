ef15cf167606f6dea7715eafc6ead9673bba4d39
willykar <103494595+willykar@users.noreply.github.com>
2024-04-22 23:59:58 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,37 +8,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-
     user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    # Fetch user data
-    user_response = requests.get(f"{url}users/{user_id}")
-    if user_response.status_code != 200:
-        print(f"Error: unable to fetch user details for ID {user_id}")
-        sys.exit(1)
-        
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
-
-    # Fetch user's TODO list
-    todos_response = requests.get(f"{url}todos?userId={user_id}")
-    if todos_response.status_code != 200:
-        print(f"Error: unable to fetch TODO list for ID {user_id}")
-        sys.exit(1)
-        
-    todos_data = todos_response.json()
-
-    # Prepare data for JSON
-    json_data = {user_id: []}
-    for todo in todos_data:
-        completed_status = todo['completed']
-        task_title = todo['title']
-        json_data[user_id].append({'task': task_title, 'completed': completed_status, 'username': employee_name})
-
-    # Write data to JSON file
-    json_filename = f"{user_id}.json"
-    with open(json_filename, 'w') as json_file:
-        json.dump(json_data, json_file)
-
-    print(f"JSON file '{json_filename}' has been created successfully.")
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
