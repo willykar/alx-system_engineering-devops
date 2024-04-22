140f845429965397dd88050dfe317f81946cdcb0
willykar <103494595+willykar@users.noreply.github.com>
2024-04-22 22:33:01 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,40 +4,24 @@ import csv
 import requests
 import sys
 
+
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
 
-    user_id = sys.argv[1]
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
+    user_data = res.json()
+    print("Employee {} is done with tasks"
+          .format(user_data.get('name')), end="")
 
-    # Fetch user data
-    user_response = requests.get(f"{url}users/{user_id}")
-    if user_response.status_code != 200:
-        print(f"Error: unable to fetch user details for ID {user_id}")
-        sys.exit(1)
-        
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
+    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            l_task.append(task)
 
-    # Fetch user's TODO list
-    todos_response = requests.get(f"{url}todos?userId={user_id}")
-    if todos_response.status_code != 200:
-        print(f"Error: unable to fetch TODO list for ID {user_id}")
-        sys.exit(1)
-        
-    todos_data = todos_response.json()
-
-    # Prepare data for CSV
-    csv_data = []
-    for todo in todos_data:
-        completed_status = "True" if todo['completed'] else "False"
-        task_title = todo['title']
-        csv_data.append([user_id, employee_name, completed_status, task_title])
-
-    # Write data to CSV file
-    csv_filename = f"{user_id}.csv"
-    with open(csv_filename, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        csv_writer.writerows(csv_data)
-
-    print(f"CSV file '{csv_filename}' has been created successfully.")
+    print("({}/{}):".format(len(l_task), len(tasks)))
+    for task in l_task:
+        print("\t {}".format(task.get("title")))
