192786574b824e9a678c5c6bc6c80bb51f59ad03
willykar <103494595+willykar@users.noreply.github.com>
2024-04-22 23:50:07 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python3
-"""
-Python script to export data in the JSON format
-Requirements:
-  Records all tasks that are owned by this employee
-"""
-
 import json
 import requests
 import sys
 
-
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
 
-    userid = sys.argv[1]
-    user = '{}users/{}'.format(url, userid)
-    res = requests.get(user)
-    json_o = res.json()
-    name = json_o.get('username')
+    user_id = sys.argv[1]
 
-    todos = '{}todos?userId={}'.format(url, userid)
-    res = requests.get(todos)
-    tasks = res.json()
-    l_task = []
-    for task in tasks:
-        dict_task = {"task": task.get('title'),
-                     "completed": task.get('completed'),
-                     "username": name}
-        l_task.append(dict_task)
+    # Fetch user data
+    user_response = requests.get(f"{url}users/{user_id}")
+    if user_response.status_code != 200:
+        print(f"Error: unable to fetch user details for ID {user_id}")
+        sys.exit(1)
+        
+    user_data = user_response.json()
+    employee_name = user_data.get('name')
 
-    d_task = {str(userid): l_task}
-    filename = '{}.json'.format(userid)
-    with open(filename, mode='w') as f:
-        json.dump(d_task, f)
+    # Fetch user's TODO list
+    todos_response = requests.get(f"{url}todos?userId={user_id}")
+    if todos_response.status_code != 200:
+        print(f"Error: unable to fetch TODO list for ID {user_id}")
+        sys.exit(1)
+        
+    todos_data = todos_response.json()
+
+    # Prepare data for JSON
+    json_data = {user_id: []}
+    for todo in todos_data:
+        completed_status = todo['completed']
+        task_title = todo['title']
+        json_data[user_id].append({'task': task_title, 'completed': completed_status, 'username': employee_name})
+
+    # Write data to JSON file
+    json_filename = f"{user_id}.json"
+    with open(json_filename, 'w') as json_file:
+        json.dump(json_data, json_file)
+
+    print(f"JSON file '{json_filename}' has been created successfully.")
