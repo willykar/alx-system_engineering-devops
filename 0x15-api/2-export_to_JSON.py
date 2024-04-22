166109703e139612fe32411dd720fbb257161a04
willykar <103494595+willykar@users.noreply.github.com>
2024-04-23 00:23:59 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,23 +5,18 @@ A Python script that uses a REST API to export employee TODO list data to a CSV 
 
 import json
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    userID = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userID)).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                         .format(userID)).json()
-    username = user.get('username')
-    tasks = []
-    for task in todos:
-        task_dict = {}
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        task_dict["username"] = username
-        tasks.append(task_dict)
-    json_obj = {}
-    json_obj[userID] = tasks
-    with open("{}.json".format(userID), 'w') as jsonfile:
-        json.dump(json_obj, jsonfile)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
