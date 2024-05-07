28f619e8676f0c346abb00eeaf53d74ff5f6477c
willykar <wilsonnjenga799@gmail.com>
2024-05-07 07:49:43 -0700

    update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-A function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit
+A function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit
 """
 import requests
 
@@ -13,20 +14,19 @@ def top_ten(subreddit):
     given subreddit
     Return None If not a valid subreddit
     """
-     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            }
 
     params = {"limit": 10}
-
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
 
     if response.status_code == 404:
         print("None")
         return
-        
     results = response.json().get("data")
 
-    [print(child.get("data").get("title")) for child in results.get("children")]
+    [print(child.get("data").get("title"))
+        for child in results.get("children")]
