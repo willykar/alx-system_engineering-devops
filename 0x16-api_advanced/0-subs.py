47a00ebeeaa2e2d16286a4b0dbc448c0cb1d7059
Kariuki79 <kariukimwangi75.com>
2024-05-07 05:29:20 -0700

    update subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     total subscribers) for a given subreddit"""
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'MyBot/0.1'}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 404:
